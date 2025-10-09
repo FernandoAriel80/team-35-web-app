21ff1e4ab9a851931f6546a5d3ce9ee7c816c6d9
isakidev <gaspar.c.developer@gmail.com>
2025-10-08 23:18:48 -0300

    chore(client): :truck: rename path

--- client/src/routes/auth/login.tsx
+++ client/src/routes/auth/login.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Login } from '../../pages/Login'
+import { Login } from '../../pages/Login/Login'
 
 export const Route = createFileRoute('/auth/login')({
   component: RouteComponent,
