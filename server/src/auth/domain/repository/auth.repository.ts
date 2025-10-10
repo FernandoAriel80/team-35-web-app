22cd65b2ea5f5401bb5736dbdde93efc7535cf11
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-10-10 20:36:23 -0300

    fix(auth): resolve validation issues
    - Add new mapper and DTO
    - Fix register/login validations
    - Improve error messages

--- server/src/auth/domain/repository/auth.repository.ts
+++ server/src/auth/domain/repository/auth.repository.ts
@@ -1,11 +1,11 @@
 import { AuthDto } from '../dto/auth.dto'
-import { CreateAuthDto } from '../dto/create-auth.dto'
+import { UserCreateValidatedDto } from '../dto/user-create-validated.dto'
 //import { UpdateAuthDto } from '../dto/update-auth.dto'
 
 export const AUTH_PG_REPOSITORY = 'AUTH_PG_REPOSITORY'
 
 export interface AuthRepository {
-  create(auth: CreateAuthDto): Promise<AuthDto>
+  create(auth: UserCreateValidatedDto): Promise<AuthDto>
   findByEmail(email: string): Promise<AuthDto | null>
   findOne(authId: number): Promise<AuthDto | null>
   /* update(auth: UpdateAuthDto, id: string): Promise<AuthDto | null>
