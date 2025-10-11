44fe0280d313a309fa544d443e354295a5ac69ec
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-10-11 15:26:31 -0300

    fix(auth-module): token validation and user data -  Add user data fetching to token renewal

--- server/src/auth/application/service/token-imple.service.ts
+++ server/src/auth/application/service/token-imple.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Request } from 'express'
 import { JwtPayloadDto } from 'src/auth/domain/dto/jwt-payload.dto'
 import { TokenValidationResultDto } from 'src/auth/domain/dto/token-validation-resultDto'
+import type { AuthRepository } from 'src/auth/domain/repository/auth.repository'
+import { AUTH_PG_REPOSITORY } from 'src/auth/domain/repository/auth.repository'
 import { TokenService } from 'src/auth/domain/service/token.service'
 
 export interface AuthenticatedRequest extends Request {
@@ -13,7 +15,11 @@ export interface AuthenticatedRequest extends Request {
 
 @Injectable()
 export class TokenImplService implements TokenService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(AUTH_PG_REPOSITORY)
+    private authPgRepository: AuthRepository,
+  ) {}
 
   async validateAndRenewToken(
     token: string,
@@ -28,6 +34,11 @@ export class TokenImplService implements TokenService {
 
     if (!userPayload) throw new UnauthorizedException('Token is invalid')
 
+    // Obtener datos completos del usuario desde la base de datos
+    const user = await this.authPgRepository.findByEmail(userPayload.email)
+    if (!user) {
+      throw new UnauthorizedException('User not found')
+    }
     const payload = {
       sub: userPayload.userId,
       email: userPayload.email,
@@ -36,9 +47,14 @@ export class TokenImplService implements TokenService {
       expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
       secret: process.env.JWT_ACCESS_SECRET,
     })
-
     return {
       new_access_token: newToken,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
       expires_in: 3600,
       valid: true,
     }
