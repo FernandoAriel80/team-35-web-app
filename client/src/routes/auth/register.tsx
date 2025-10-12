410cdd1781b7ef4efab2e1afcf5a77326bf195d0
isakidev <gaspar.c.developer@gmail.com>
2025-10-12 13:37:41 -0300

    feat(client): :sparkles: add protected route

--- client/src/routes/auth/register.tsx
+++ client/src/routes/auth/register.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 import { Register } from '../../pages/Register/Register'
 
 export const Route = createFileRoute('/auth/register')({
   component: RouteComponent,
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({ to: '/' })
+    }
+  },
 })
 
 function RouteComponent() {
