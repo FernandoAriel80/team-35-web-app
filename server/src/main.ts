cf24f5487eb297555fb0fb0970d07e5789714ab2
lopezandres07 <andlop17@gmail.com>
2025-10-13 22:49:36 -0300

    fix(server): :sparkles: enable CORS in the main application bootstrap

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build()
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.enableCors()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
   await app.listen(process.env.PORT ?? 3000)
